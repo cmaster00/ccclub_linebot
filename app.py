0e1058cf455f47862d581a65c250b2517cb53114
stanly <dk3vup12018@gmail.com>
2023-06-27 23:42:30 +0800

    version1

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(
+    'mhxy80KfcTKKVzJRuSlKJrfRaPz2QAujoIxXcqLOHlLDGVOnBfd0nhZrlFoGuvdiE4zv0t6sPb3bvPM4V1R9cGOa34GNeK5yYfknPc7KBc7oWejaACPtrdvUCooAHnwqaHaHA1V7ZO2JGOFZH7wwBgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('3c47f560a37827f9a1a9063e35a3511a')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    r = '很抱歉，您說什麼?'
+
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
+
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
+
+
+if __name__ == "__main__":
+    app.run()
+
