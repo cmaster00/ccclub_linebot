256ba9ae329b3fb8e2e4f1699e91faf7ddfad5d3
stanly <dk3vup12018@gmail.com>
2023-06-28 10:48:18 +0800

    update_crawl

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
+import requests
+import json
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -34,6 +37,55 @@ def callback():
 
     return 'OK'
 
+def cashback():
+    url = 'https://rich01.com/best-only-cashback-credit-cards/'
+    r = requests.get(url, verify=False)
+
+    if r.status_code == requests.codes.ok:
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        table = soup.find('table', style="border-collapse: collapse; width: 100%; height: 970px;")
+
+        trs = table.find_all('tr')
+        for tr in trs[1:6]:
+            tds = tr.find_all('td')
+            for td in tds[0]:
+                card_name = td.text
+                print(card_name)
+            for td in tds[1]:
+                card_fdback = td.text
+                print(card_fdback)
+
+        question = soup.find('div', id="sp-eap-accordion-section-18093").find('script',
+                                                                              type="application/ld+json").get_text()
+        questiontojson = json.loads(question, strict=False)
+        qa_info = questiontojson['mainEntity']
+        for qa in qa_info:
+            print(qa['name'])
+            print(qa['acceptedAnswer']['text'])
+    else:
+        print("Can't get the website")
+
+
+def credit_cashback():
+    url_2 = 'https://rich01.com/e-commerce-credit-card/'
+    r = requests.get(url_2, verify=False)
+
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        title = soup.find()
+        tables = soup.find_all('table')
+        for table in tables[4:7]:
+            trs = table.find_all('tr')
+            for tr in trs[2:len(trs) - 1]:
+                tds = tr.find_all('td')
+                for td in tds[0]:
+                    print(td.text)
+                for td in tds[1]:
+                    print(td.text)
+    else:
+        print("Can't get the website")
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -52,7 +104,7 @@ def handle_message(event):
         return
 
     if msg in ['hi', 'Hi']:
-        r = '嗨'
+        cashback()
     elif msg == '你吃飯了嗎':
         r = '還沒'
     elif msg == '你是誰':
