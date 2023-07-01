4e72c0490fdfb241e566abe52873d084b2a013f2
stanly <dk3vup12018@gmail.com>
2023-07-01 14:25:12 +0800

    update0701

--- scraper.py
+++ scraper.py
@@ -8,6 +8,10 @@ def cashback():
     url = 'https://rich01.com/best-only-cashback-credit-cards/'
     r = requests.get(url, verify=False)
 
+    name = []
+    feedback = []
+    content = ""
+
     if r.status_code == requests.codes.ok:
 
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -18,19 +22,30 @@ def cashback():
             tds = tr.find_all('td')
             for td in tds[0]:
                 card_name = td.text
-                print(card_name)
+                a = card_name.strip('\n')
+                if a == '':
+                    continue
+                else:
+                    name.append(a)
             for td in tds[1]:
                 card_fdback = td.text
-                print(card_fdback)
+                feedback.append(card_fdback.strip('\n'))
+        print(feedback)
+        for i in range(4):
+            if feedback[i] == '':
+                continue
+            else:
+                content += "{}\n{}\n".format(name[i], feedback[i])
+        print(content)
 
 
-        question = soup.find('div', id="sp-eap-accordion-section-18093").find('script',
-                                                                              type="application/ld+json").get_text()
-        questiontojson = json.loads(question, strict=False)
-        qa_info = questiontojson['mainEntity']
-        for qa in qa_info:
-            qa_name = qa['name']
-            qa_answer = qa['acceptedAnswer']['text']
+        # question = soup.find('div', id="sp-eap-accordion-section-18093").find('script',
+        #                                                                       type="application/ld+json").get_text()
+        # questiontojson = json.loads(question, strict=False)
+        # qa_info = questiontojson['mainEntity']
+        # for qa in qa_info:
+        #     qa_name = qa['name']
+        #     qa_answer = qa['acceptedAnswer']['text']
 
     else:
         print("Can't get the website")
@@ -55,7 +70,4 @@ def credit_cashback():
     else:
         print("Can't get the website")
 
-
-if __name__ == "__main__":
-    cashback()
-    # credit_cashback()
+cashback()
